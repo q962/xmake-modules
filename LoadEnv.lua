5904558200177fb61e64f1e798cf1cc3de341b04
提及 <q962_@outlook.com>
2024-09-28 20:34:27 +0800

    fix: 修复 LoadEnv 关于环境变值的处理
    完善前置后置覆盖的语法

--- LoadEnv.lua
+++ LoadEnv.lua
@@ -2,16 +2,25 @@ import("core.project.config")
 
 function _load(target, env_file)
     env_file = env_file:gsub("%.%.", ".")
-
     try {function()
         local data = io.load(env_file)
         if data then
             for name, value in pairs(data) do
+                local new_env_value = "";
+
+                local old_value = os.getenv(name);
+
                 if name:sub(#name) == ':' then
                     name = name:sub(1, #name - 1)
-                    value = path.joinenv(value, os.getenv(name))
+                    new_env_value = value .. path.envsep() .. (os.getenv(name) or "")
+                elseif name:sub(1) == ':' then
+                    name = name:sub(2)
+                    new_env_value = (os.getenv(name) or "") .. path.envsep() .. value
+                else
+                    new_env_value = value
                 end
-                target:set('runenv', name, value)
+
+                os.setenv(name, new_env_value)
             end
         end
     end}
