85c531fdf6b2062e009fd3e508f102128daf7c10
q962 <arcere@qq.com>
2026-01-01 22:48:06 +0800

    feat: 调整 gres 的生成规则

--- rules/gnome/xmake.lua
+++ rules/gnome/xmake.lua
@@ -27,10 +27,14 @@ do
         local gnome = import("../../gnome")
         local utils = import("../../utils")
 
-        if is_mode("release") then
-            gnome.compile_resources(target, "app", target:scriptdir() .. "/res/gresources.xml", {"--manual-register"})
-        else
-            target:add("defines", "app_register_resource=void app_register_resource")
+        for _, filepath in ipairs(os.files(target:scriptdir() .. "/res/*.gres.xml")) do
+            local basename = path.basename(path.basename(filepath))
+
+            if is_mode("release") then
+                gnome.compile_resources(target, basename, filepath, {"--manual-register"})
+            else
+                target:add("defines", basename .. "_register_resource=void " .. basename .. "_register_resource")
+            end
         end
 
         if get_config("APPID") then
@@ -53,30 +57,34 @@ do
         import("core.base.xml")
 
         local sep = is_subhost("windows") and ";" or ":"
-        local gresource = xml.loadfile(target:scriptdir() .. "/res/gresources.xml")
+
         local G_RESOURCE_OVERLAYS = ""
 
-        for gresource_index in ipairs(gresource.children) do
-            local gresource_value = gresource.children[gresource_index]
-            if (gresource_value.name == "gresource") then
+        for _, filepath in ipairs(os.files(target:scriptdir() .. "/res/*.gres.xml")) do
+            local gresource = xml.loadfile(filepath)
 
-                local prefix = gresource_value.attrs.prefix;
+            for gresource_index in ipairs(gresource.children) do
+                local gresource_value = gresource.children[gresource_index]
+                if (gresource_value.name == "gresource") then
 
-                for file_index in ipairs(gresource_value.children) do
-                    local file_value = gresource_value.children[file_index]
-                    if (file_value.name == "file") then
-                        local file_path = file_value.children[1].text;
+                    local prefix = gresource_value.attrs.prefix;
 
-                        local alias = file_value.attrs and file_value.attrs.alias or nil;
+                    for file_index in ipairs(gresource_value.children) do
+                        local file_value = gresource_value.children[file_index]
+                        if (file_value.name == "file") then
+                            local file_path = file_value.children[1].text;
 
-                        local res_path = prefix .. "/" .. (alias and alias or file_path)
+                            local alias = file_value.attrs and file_value.attrs.alias or nil;
 
-                        if #G_RESOURCE_OVERLAYS > 0 then
-                            G_RESOURCE_OVERLAYS = G_RESOURCE_OVERLAYS .. sep
+                            local res_path = prefix .. "/" .. (alias and alias or file_path)
+
+                            if #G_RESOURCE_OVERLAYS > 0 then
+                                G_RESOURCE_OVERLAYS = G_RESOURCE_OVERLAYS .. sep
+                            end
+
+                            G_RESOURCE_OVERLAYS = G_RESOURCE_OVERLAYS .. res_path .. "=" ..
+                                                      path.absolute(file_path, target:scriptdir() .. "/res/");
                         end
-
-                        G_RESOURCE_OVERLAYS = G_RESOURCE_OVERLAYS .. res_path .. "=" ..
-                                                  path.absolute(file_path, target:scriptdir() .. "/res/");
                     end
                 end
             end
